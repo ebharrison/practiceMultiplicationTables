bf68768ed1dbc0d8c5b2b8bf8665cc8a5819e7a4
sensishadow818 <magicshadow818@gmail.com>
2019-01-21 21:17:00 -0800

    Replaced background code with a single function. Now whenever a page wants the background, call function with controller, and object itself. Also, deleted errenous code. Start page has weird code with setting background image as image for label. Removed this code.

--- functionalMathAi.py
+++ functionalMathAi.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import font as tkfont
 import PIL as pillow
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 import random
 import time
 
@@ -31,6 +31,13 @@ import time
 '''
 
 
+def display_background(controller, frame):
+    chalkboard = controller.chalkboard
+    background = tk.Label(frame, image=chalkboard)
+    background.image = chalkboard
+    background.place(x=0, y=0, relwidth=1, relheight=1)
+
+
 class Application(tk.Tk):
     def show_and_update_frame(self, page_name):
         '''Show and update a frame for the given page name'''
@@ -41,8 +48,9 @@ class Application(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
 
-
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+
+        self.chalkboard = ImageTk.PhotoImage(Image.open("chalkboard.jpg"))
 
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
@@ -53,7 +61,7 @@ class Application(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
-        #WHERE YOU PUT ALL PAGES
+        # WHERE YOU PUT ALL PAGES
         for F in (StartPage, problem_page, answer_page, statistic_page):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
@@ -66,39 +74,38 @@ class Application(tk.Tk):
 
         self.show_frame("StartPage")
 
-        self.bind('<Return>',lambda anon: self.show_and_update_frame('answer_page'))
-
-
+        self.bind('<Return>', lambda anon: self.show_and_update_frame('answer_page'))
 
     def show_frame(self, page_name):
         '''Show a frame for the given page name'''
         frame = self.frames[page_name]
         frame.tkraise()
 
-    def get_page(self,page_class):
+    def get_page(self, page_class):
         return self.frames[page_class]
 
+
 class StartPage(tk.Frame):
-    chalkboard='reference to image'
+    chalkboard = 'reference to image'
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent, background="saddle brown")
         self.controller = controller
 
-        chalkboard = ImageTk.PhotoImage(Image.open("chalkboard.jpg"))
-        background = tk.Label(self, image=chalkboard)
-        background.image = chalkboard
-        background.place(x=0,y=0,relwidth=1,relheight=1)
+        # chalkboard = controller.chalkboard
+        # background = tk.Label(self, image=chalkboard)
+        # background.image = chalkboard
+        # background.place(x=0, y=0, relwidth=1, relheight=1)
 
-        intro = tk.Label(self, text="Welcome to Productive!", font=(controller.title_font,40),
-                         borderwidth=0,fg="white",bg="#2d4630")
-        intro.image = chalkboard
-        #intro.pack()
-        intro.place(x=80,y=20)
+        display_background(controller, self)
+
+        intro = tk.Label(self, text="Welcome to Productive!", font=(controller.title_font, 40),
+                         borderwidth=0, fg="white", bg="#2d4630")
+        intro.place(x=80, y=20)
 
         start_button = tk.Button(self, text="Begin!", command=lambda: controller.show_and_update_frame("problem_page"),
                                  bd=0)
-        start_button.place(x=280,y=365)
+        start_button.place(x=280, y=365)
 
 
 class problem_page(tk.Frame):
@@ -106,42 +113,44 @@ class problem_page(tk.Frame):
         return random.choice(set)
 
     def solve_problem(problem):
-        #problem is a tuple
-        return problem[0]*problem[1]
+        # problem is a tuple
+        return problem[0] * problem[1]
 
     def solve_problem(problem):
-        #problem is a tuple
-        return problem[0]*problem[1]
+        # problem is a tuple
+        return problem[0] * problem[1]
 
     def format_problem(problem):
-        #problem is a tuple
-        return str(problem[0])+' times '+str(problem[1])
+        # problem is a tuple
+        return str(problem[0]) + ' times ' + str(problem[1])
 
-    user_answer=-1
-    start_time=-1
+    user_answer = -1
+    start_time = -1
 
-    number_problems=3
-    time_per_problem=5
-    #bounds are inclusive
-    lower_bound=1
-    upper_bound=12
+    number_problems = 3
+    time_per_problem = 5
 
-    #initalize wrong problems to have one copy of every possible problem
-    #inclusive bounds
-    wrong_problems=[]
-    for i in range(lower_bound,upper_bound+1):
-        for j in range(lower_bound,upper_bound+1):
-            wrong_problems.append((i,j))
+    # bounds are inclusive
+
+    lower_bound = 1
+    upper_bound = 12
+
+    # initalize wrong problems to have one copy of every possible problem
+    # inclusive bounds
+    wrong_problems = []
+    for i in range(lower_bound, upper_bound + 1):
+        for j in range(lower_bound, upper_bound + 1):
+            wrong_problems.append((i, j))
 
     problem = 'tuple'
     correct_answer = 'integer product of tuple,problem, '
 
-    prompt='Label'
-    input_box='Entry'
-    error_box='Label'
+    prompt = 'Label'
+    input_box = 'Entry'
+    error_box = 'Label'
 
-    def validate(self,name,mode):
-        ans=problem_page.user_answer.get()
+    def validate(self, name, mode):
+        ans = problem_page.user_answer.get()
         try:
             if ans:
                 int(ans)
@@ -152,6 +161,8 @@ class problem_page(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
+
+        display_background(controller, self)
 
         question = tk.Label(self, text='What is the product of', font=controller.title_font)
         question.pack(side="top", fill="x", pady=10)
@@ -166,13 +177,13 @@ class problem_page(tk.Frame):
         problem_page.error_box = tk.Label(self, text='', font=controller.title_font)
         problem_page.error_box.pack(side="top", fill="x", pady=10)
 
-        problem_page.user_answer=tk.StringVar()
-        user_answer=problem_page.user_answer
+        problem_page.user_answer = tk.StringVar()
+        user_answer = problem_page.user_answer
         user_answer.trace('w', problem_page.validate)
 
-        problem_page.input_box=tk.Entry(self,textvariable=problem_page.user_answer)
+        problem_page.input_box = tk.Entry(self, textvariable=problem_page.user_answer)
         problem_page.input_box.pack(side="top", fill="x", pady=10)
-        #problem_page.input_box.focus()
+        # problem_page.input_box.focus()
 
         '''
             i think i need to bind return to entry box and then grade. Question then
@@ -181,17 +192,17 @@ class problem_page(tk.Frame):
 
         problem_page.prompt.config(text=problem_page.format_problem(problem_page.problem))
 
-        problem_page.start_time=time.time()
+        problem_page.start_time = time.time()
 
         # def update_answer_page():
         #     answer_page.update()
         #     controller.show_frame("answer_page")
 
-        submit_button = tk.Button(self, text="Grade", command=lambda:controller.show_and_update_frame("answer_page"))
+        submit_button = tk.Button(self, text="Grade", command=lambda: controller.show_and_update_frame("answer_page"))
         submit_button.pack()
 
     def update(self):
-        problem_page.user_answer=tk.StringVar()
+        problem_page.user_answer = tk.StringVar()
         problem_page.user_answer.trace('w', problem_page.validate)
 
         problem_page.input_box.config(textvariable=problem_page.user_answer)
@@ -201,81 +212,84 @@ class problem_page(tk.Frame):
         problem_page.correct_answer = problem_page.solve_problem(problem_page.problem)
 
         problem_page.prompt.config(text=problem_page.format_problem(problem_page.problem))
-        problem_page.start_time=time.time()
+        problem_page.start_time = time.time()
 
 
 class answer_page(tk.Frame):
-
-    response='Label'
-    solution='Label'
-    count_correct_solutions=0
-    repeat_button='Button'
-    controller='Controller'
-    problem_count=0
+    response = 'Label'
+    solution = 'Label'
+    count_correct_solutions = 0
+    repeat_button = 'Button'
+    controller = 'Controller'
+    problem_count = 0
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
         statistic_page.controller = controller
 
-        #two labels
-        answer_page.response=tk.Label(self,text="response")
+        display_background(controller, self)
+
+        # two labels
+        answer_page.response = tk.Label(self, text="response")
         answer_page.response.pack()
 
-        answer_page.solution=tk.Label(self,text="solution")
+        answer_page.solution = tk.Label(self, text="solution")
         answer_page.solution.pack()
 
-        answer_page.repeat_button=tk.Button(self,text="Again",
-            command=lambda: controller.show_and_update_frame("problem_page"))
+        answer_page.repeat_button = tk.Button(self, text="Again",
+                                              command=lambda: controller.show_and_update_frame("problem_page"))
         answer_page.repeat_button.pack()
 
     def update(self):
-        problem=problem_page.format_problem(problem_page.problem)
-        user_answer=int(problem_page.user_answer.get())
-        correct_answer=problem_page.correct_answer
-        start_time=problem_page.start_time
-        time_elapsed=time.time()-start_time
+        problem = problem_page.format_problem(problem_page.problem)
+        user_answer = int(problem_page.user_answer.get())
+        correct_answer = problem_page.correct_answer
+        start_time = problem_page.start_time
+        time_elapsed = time.time() - start_time
 
-        answer_page.problem_count+=1
-        if answer_page.problem_count==problem_page.number_problems:
+        answer_page.problem_count += 1
+        if answer_page.problem_count == problem_page.number_problems:
             answer_page.repeat_button.config(command=lambda:
-                statistic_page.controller.show_and_update_frame('statistic_page'))
+            statistic_page.controller.show_and_update_frame('statistic_page'))
 
-        if time_elapsed<problem_page.time_per_problem:
-            if user_answer==correct_answer:
+        if time_elapsed < problem_page.time_per_problem:
+            if user_answer == correct_answer:
                 # user gave correct solution
                 answer_page.response.config(text="Correct!")
-                answer_page.solution.config(text=problem+" is "+str(correct_answer))
-                answer_page.count_correct_solutions+=1
+                answer_page.solution.config(text=problem + " is " + str(correct_answer))
+                answer_page.count_correct_solutions += 1
             else:
-                #user gave wrong solution
+                # user gave wrong solution
                 answer_page.response.config(text="Sorry, that was not correct")
-                answer_page.solution.config(text=problem+" is "+str(correct_answer))
+                answer_page.solution.config(text=problem + " is " + str(correct_answer))
         else:
             answer_page.response.config(text="You ran out of time")
-            answer_page.solution.config(text=problem+" is "+str(correct_answer))
-            
+            answer_page.solution.config(text=problem + " is " + str(correct_answer))
+
 
 class statistic_page(tk.Frame):
-
-    response='Label'
+    response = 'Label'
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         self.controller = controller
 
-        statistic_page.response=tk.Label(self,text="score not displaying",font=controller.title_font)
+        display_background(controller, self)
+
+        statistic_page.response = tk.Label(self, text="score not displaying", font=controller.title_font)
         statistic_page.response.pack()
 
     def update(self):
-        score=(answer_page.count_correct_solutions/problem_page.number_problems)*100
-        statistic_page.response.config(text="Your score is "+str( round(score,2) )+"%")
+        score = (answer_page.count_correct_solutions / problem_page.number_problems) * 100
+        statistic_page.response.config(text="Your score is " + str(round(score, 2)) + "%")
+
 
 if __name__ == "__main__":
     app = Application()
 
     app.geometry("599x400")
-    app.resizable(False,False)
+    app.resizable(False, False)
     app.title("Productive-the multiplication app")
 
     app.mainloop()
