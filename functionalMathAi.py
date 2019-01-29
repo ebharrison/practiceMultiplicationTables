120e84e77ed1d928f2c4c50bee3b043d33794915
sensishadow818 <magicshadow818@gmail.com>
2019-01-28 21:25:29 -0800

    Enabled enter key and better label placement (problems with vim)

--- functionalMathAi.py
+++ functionalMathAi.py
@@ -27,10 +27,13 @@ import time
     -buttons can have images too!!!
     
     -reuse same image on all pages for ease of consistent size
+    
+    ! use pack and pady to move label down, 
 '''
 
 blackboard_green = "#2d4630"
 name = "Productivity "
+
 
 def display_background(controller, frame):
     chalkboard = controller.chalkboard
@@ -40,11 +43,36 @@ def display_background(controller, frame):
 
 
 class Application(tk.Tk):
+    def show_frame(self, page_name):
+        '''Show a frame for the given page name'''
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+    def get_page(self, page_class):
+        return self.frames[page_class]
+
     def show_and_update_frame(self, page_name):
         '''Show and update a frame for the given page name'''
         frame = self.frames[page_name]
         frame.update()
         frame.tkraise()
+        self.current_page = page_name
+
+    def enter_key_progress(self):
+        curr_index = self.index[self.current_page]
+        curr_index += 1
+
+        new_page = ""
+        try:
+            new_page = self.index[curr_index]
+        except KeyError:
+            new_page = "statistic_page"
+
+        if new_page == "statistic_page":
+            if answer_page.problem_count < problem_page.number_problems:
+                new_page = "problem_page"
+
+        self.show_and_update_frame(new_page)
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -52,6 +80,8 @@ class Application(tk.Tk):
         self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
 
         self.chalkboard = ImageTk.PhotoImage(Image.open("chalkboard.jpg"))
+
+        self.current_page = "start_page"
 
         # the container is where we'll stack a bunch of frames
         # on top of each other, then the one we want visible
@@ -61,32 +91,30 @@ class Application(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
+        self.index = {}
         self.frames = {}
+
         # WHERE YOU PUT ALL PAGES
-        for F in (StartPage, problem_page, answer_page, statistic_page):
+        counter = 0
+        for F in (start_page, problem_page, answer_page, statistic_page):
             page_name = F.__name__
             frame = F(parent=container, controller=self)
             self.frames[page_name] = frame
+
+            self.index[page_name] = counter
+            self.index[counter] = page_name
+            counter += 1
 
             # put all of the pages in the same location;
             # the one on the top of the stacking order
             # will be the one that is visible.
             frame.grid(row=0, column=0, sticky="nsew")
 
-        self.show_frame("StartPage")
-
-        self.bind('<Return>', lambda anon: self.show_and_update_frame('answer_page'))
-
-    def show_frame(self, page_name):
-        '''Show a frame for the given page name'''
-        frame = self.frames[page_name]
-        frame.tkraise()
-
-    def get_page(self, page_class):
-        return self.frames[page_class]
+        self.show_frame(self.current_page)
+        self.bind('<Return>', lambda anon: self.enter_key_progress())
 
 
-class StartPage(tk.Frame):
+class start_page(tk.Frame):
     chalkboard = 'reference to image'
 
     def __init__(self, parent, controller):
@@ -100,15 +128,14 @@ class StartPage(tk.Frame):
 
         display_background(controller, self)
 
-        intro = tk.Label(self, text="Welcome to "+name+"!", font=(controller.title_font, 40),
+        intro = tk.Label(self, text="Welcome to " + name + "!", font=(controller.title_font, 40),
                          borderwidth=0, fg="white", bg=blackboard_green)
-        intro.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+        intro.pack(side="top", pady=30)
 
         start_button = tk.Button(self, highlightbackground=blackboard_green, text="Ready!",
-                                 command=lambda: controller.show_and_update_frame("problem_page"),
-                                 bd=0)
+                                 command=lambda: controller.show_and_update_frame("problem_page"))
 
-        start_button.place(x=280, y=250)
+        start_button.pack(side="bottom", pady=25)
 
 
 class problem_page(tk.Frame):
@@ -167,8 +194,9 @@ class problem_page(tk.Frame):
 
         display_background(controller, self)
 
-        question = tk.Label(self, text='What is the product of', font=controller.title_font)
-        question.pack(side="top", fill="x", pady=10)
+        question = tk.Label(self, text="What is the product of", font=(controller.title_font, 40),
+                            borderwidth=0, fg="white", bg=blackboard_green)
+        question.pack(side="top", pady=12)
 
         '''
             One label is used to display all problems. The text on the label is
@@ -196,10 +224,6 @@ class problem_page(tk.Frame):
         problem_page.prompt.config(text=problem_page.format_problem(problem_page.problem))
 
         problem_page.start_time = time.time()
-
-        # def update_answer_page():
-        #     answer_page.update()
-        #     controller.show_frame("answer_page")
 
         submit_button = tk.Button(self, text="Grade", command=lambda: controller.show_and_update_frame("answer_page"))
         submit_button.pack()
@@ -246,7 +270,13 @@ class answer_page(tk.Frame):
 
     def update(self):
         problem = problem_page.format_problem(problem_page.problem)
-        user_answer = int(problem_page.user_answer.get())
+
+        user_answer = None
+        try:
+            user_answer = int(problem_page.user_answer.get())
+        except ValueError:
+            pass
+
         correct_answer = problem_page.correct_answer
         start_time = problem_page.start_time
         time_elapsed = time.time() - start_time
@@ -281,7 +311,7 @@ class statistic_page(tk.Frame):
         display_background(controller, self)
 
         statistic_page.response = tk.Label(self, text="score not displaying", font=controller.title_font)
-        statistic_page.response.place(relx=0.5,rely=0.5,anchor=tk.CENTER)
+        statistic_page.response.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
     def update(self):
         score = (answer_page.count_correct_solutions / problem_page.number_problems) * 100
@@ -293,7 +323,6 @@ if __name__ == "__main__":
 
     app.geometry("599x400")
     app.resizable(False, False)
-    app.title(name+"- the multiplication app")
+    app.title(name + "- the multiplication app")
 
     app.mainloop()
-
